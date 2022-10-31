a538e4bc203a38ff1b48245a618ead633ab765e1
20170856MS <fpqk1004@sungkyul.ac.kr>
2022-10-31 14:27:23 +0900

    1031

---
+++ src/main/java/com/iu/home/StudyPApplication.java
@@ -0,0 +1,13 @@
+package com.iu.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPApplication.class, args);
+	}
+
+}
